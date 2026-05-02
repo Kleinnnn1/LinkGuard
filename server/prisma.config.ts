087864cccad0aa361e07ef5d8ea0dc955af79065
KennBalino <kennbalino@gmail.com>
2026-05-02 10:57:21 +0800

    prisma was installed in client not in the server

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
@@ -6,7 +7,7 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL,
+    url: process.env["DATABASE_URL"]!,
+    directUrl: process.env["DIRECT_URL"]!,
   },
 });
